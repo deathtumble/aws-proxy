7977169054893a8d7897b0c8018d9f1fdb7cd176
Murray Tait <death.tumble@gmail.com>
2018-07-22 08:16:18 +0100

    remove unused import

--- src/main/java/uk/co/murraytait/awsproxy/CloudServiceAdaptor.java
+++ src/main/java/uk/co/murraytait/awsproxy/CloudServiceAdaptor.java
@@ -2,7 +2,6 @@ package uk.co.murraytait.awsproxy;
 
 import java.net.UnknownHostException;
 import java.util.Collection;
-import java.util.SortedSet;
 
 import uk.co.murraytait.awsproxy.model.ServiceSummary;
 
